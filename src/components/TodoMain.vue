1444d67aaf7aebe0e1fed0ab70cc03c06580ac72
kimsungmin1011 <tata153@naver.com>
2023-02-09 14:52:20 +0900

    Feat: Add edit, del function

--- src/components/TodoMain.vue
+++ src/components/TodoMain.vue
@@ -3,16 +3,29 @@
     <div class="page">
     <header><h1>Vue Fire todo1</h1></header>
     <main>
+
       <div class="todos">
-        <div class="write">
-          <input type="text" v-model="addItemText"/>
+        <div class="write" v-if="writeState==='add'"> <!--등록-->
+          <input ref="writeArea" type="text" v-model="addItemText"/>
           <button @click="addItem" class="btn add">Add</button>
         </div>
+
+        <div class="write" v-else> <!--수정-->
+            <input ref="writeArea" type="text" v-model="editItemText"/>
+          <button @click="editSave" class="btn add">Save</button>
+        </div>
+
         <ul class="list">
             <ul>
-                <li v-for="item in todos" :key="item.text">
-                    <i :class="[item.state === 'yet'?'far':'fas', 'fa-check-square']"></i>
-                    <strong>{{ item.text }}</strong>        
+                <li v-for="(item,i) in todos" :key="item.text">
+                    <i @click="checkItem(i)" :class="[item.state === 'yet'?'far':'fas', 'fa-check-square']"></i>
+                    <strong>{{ item.text }}</strong>     
+                    <span>
+                        <b>
+                            <a href="" @click.prevent="editShow(i)">Edit</a>
+                            <a href="" @click.prevent="delShow(i)">Del</a>
+                        </b>
+                    </span>
                 </li>
             </ul>
         </ul>
@@ -26,7 +39,10 @@
 export default {
     data() {
         return {
+        writeState:'add',
+        crrEditItem: '',
         addItemText: '',
+        editItemText:'',
         todos:[
         {text: '공부하기', state: 'yet'},
         {text: '운동하기', state: 'done'},
@@ -35,10 +51,34 @@ export default {
         }
     },
     methods: {
-    addItem() {
-        this.todos.push({text: this.addItemText, state: 'yet'},)
+        addItem() {
+            if (this.addItemText==='') return;
+            this.todos.push({text: this.addItemText, state: 'yet'});
+            this.addItemText = '';
+        },
+        checkItem(i) {
+            if (this.todos[i].state==='yet') 
+                {this.todos[i].state==='done'
+            } else if (this.todos[i].state==='done') 
+                {this.todos[i].state==='yet'}
+        },
+        editShow(index) {
+            this.crrEditItem=index;
+            this.writeState='edit';
+            this.editItemText=this.todos[index].text
+        },
+        editSave() {
+            this.todos[this.crrEditItem].text = this.editItemText;
+            this.writeState='add'
+        },
+        delShow(index) {
+            this.todos.splice(index, 1)
+        },
+        
+    },
+    mounted() {
+        this.$refs.writeArea.focus();
     }
-}
 }
 </script>
 
